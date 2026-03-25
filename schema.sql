4e855f028118e5daf60f2931c04e4e222a726d8a
Matias Tamminen <60567287+MatiasOETamminen@users.noreply.github.com>
2026-03-25 20:18:49 +0200

    Refactor database schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,18 @@
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE albums (
+    name TEXT,
+    artist_id INTEGER NOT NULL,
+    year INTEGER,
+    cover BLOB,
+    songlist TEXT,
+    PRIMARY KEY (name, artist_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
@@ -6,19 +21,15 @@ CREATE TABLE users (
 
 CREATE TABLE reviews (
     id INTEGER PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    album_id INTEGER REFERENCES albums,
+    user_id INTEGER NOT NULL,
+    album_name TEXT NOT NULL,
+    album_artist_id INTEGER NOT NULL,
     content TEXT,
-    grade INTEGER
-);
-
-CREATE TABLE albums (
-    name TEXT,
-    artist_id INTEGER REFERENCES artists,
-    year INTEGER,
-    cover BLOB,
-    songlist TEXT,
-    PRIMARY KEY (name, artist_id)
+    grade INTEGER,
+    sent_at TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (album_name, album_artist_id)
+        REFERENCES albums(name, artist_id)
 );
 
 CREATE TABLE genres (
@@ -27,19 +38,22 @@ CREATE TABLE genres (
 );
 
 CREATE TABLE albumgenres (
-    album_id INTEGER REFERENCES albums,
-    genre_id INTEGER REFERENCES genres,
-    PRIMARY KEY (album_id, genre_id)
-);
-
-CREATE TABLE artists (
-    id INTEGER PRIMARY KEY,
-    name TEXT UNIQUE
+    album_name TEXT NOT NULL,
+    album_artist_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    PRIMARY KEY (album_name, album_artist_id, genre_id),
+    FOREIGN KEY (album_name, album_artist_id)
+        REFERENCES albums(name, artist_id),
+    FOREIGN KEY (genre_id)
+        REFERENCES genres(id)
 );
 
 CREATE TABLE comments (
     id INTEGER PRIMARY KEY,
-    review_id INTEGER REFERENCES reviews,
-    user_id INTEGER REFERENCES users,
-    content TEXT
+    review_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT,
+    sent_at TEXT,
+    FOREIGN KEY (review_id) REFERENCES reviews(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
